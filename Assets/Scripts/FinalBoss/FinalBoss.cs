2de43ad40270cc6c79fe09c5aae3498b816cdc54
dmurayama93 <dylan@gamedevhq.com>
2020-12-11 13:26:20 -1000

    conflict resolve

--- Assets/Scripts/FinalBoss/FinalBoss.cs
+++ Assets/Scripts/FinalBoss/FinalBoss.cs
@@ -59,6 +59,7 @@ public class FinalBoss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.rotation = Quaternion.Euler(0, 90, 0);
         //assigning current hp
         _curHp = _maxHp;
 
